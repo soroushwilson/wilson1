edd4682eaf6db987f236c04f9342d7c32ab56041
soroushwilson <soroushwilson@users.noreply.github.com>
2016-09-08 19:16:03 +0430

    Update fantasty_writter.lua

--- plugins/fantasty_writter.lua
+++ plugins/fantasty_writter.lua
@@ -103,7 +103,7 @@ local function run(msg, matches)
 		a=a+1
 		result_text = result_text..a.."- "..result[a].."\n\n"
 	end
-	return result_text.."______________________________\n@BeyondTeam"
+	return result_text.."______________________________\nsoroush_team"
 end
 
 return {
