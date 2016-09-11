cdb940fc7bf88a85bbe9b5155f175923e471cc8d
soroushwilson <soroushwilson@users.noreply.github.com>
2016-09-11 20:23:11 +0430

    Update shieldbot.lua

--- bot/shieldbot.lua
+++ bot/shieldbot.lua
@@ -251,28 +251,20 @@ function create_config( )
     "sh",
     "serverinfo"
     },
-    sudo_users = {80182995},--Sudo users
+    sudo_users = {140760293},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[MeGa shield v2.9
+    about_text = [[soroush v2.9
 An advanced administration bot based on TG-CLI written in Lua
 
 Github:
-https://github.com/hafez16/mega-shield
+https://github.com/soroushwilson/wilson1
 
 Admins:
-@hafez1116hafez [Developer]
-@amirho3ien911 [Developer]
-@Eblis_alone [Manager]
-@Mmd_hei [sponsor]
+soroush_wilson
 
 Special thanks to
-SEEDTEAM
-Hextor team
-Magic team
-Avira team
+soroush
 
-Our channels
-@shieldTM [persian]
 ]],
     help_text_realm = [[
 Realm Commands:
